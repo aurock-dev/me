19f5051f07e08ba668111ab0127cb14f85111187
aurock <jeremy24200@gmail.com>
2022-04-01 05:57:01 +0200

    🚧

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ $('.tab-content-3').hide();
 $('.tab1').click(function(){
     $('.tab1').addClass('tab-btn-active');
     $('.tab-content-1').show();
+
     $('.tab2').removeClass('tab-btn-active');
     $('.tab-content-2').hide();
+
     $('.tab3').removeClass('tab-btn-active');
     $('.tab-content-3').hide();
 });
@@ -13,8 +15,10 @@ $('.tab1').click(function(){
 $('.tab2').click(function(){
     $('.tab1').removeClass('tab-btn-active');
     $('.tab-content-1').hide();
+
     $('.tab2').addClass('tab-btn-active');
     $('.tab-content-2').show();
+
     $('.tab3').removeClass('tab-btn-active');
     $('.tab-content-3').hide();
 });
@@ -22,8 +26,10 @@ $('.tab2').click(function(){
 $('.tab3').click(function(){
     $('.tab1').removeClass('tab-btn-active');
     $('.tab-content-1').hide();
+
     $('.tab2').removeClass('tab-btn-active');
     $('.tab-content-2').hide();
+    
     $('.tab3').addClass('tab-btn-active');
     $('.tab-content-3').show();
 });
